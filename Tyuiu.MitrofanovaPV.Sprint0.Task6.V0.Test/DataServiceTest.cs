89306d9ff453862617b15f2a98af472f8c6327da
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-09-16 23:27:04 +0500

    Task6

---
+++ Tyuiu.MitrofanovaPV.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint0.Task6.V0.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        { var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+        Assert.AreEqual(15, res);}
+        [TestMethod]
+        public void ChecSubtractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+        [TestMethod]
+        public void CheckMultArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
