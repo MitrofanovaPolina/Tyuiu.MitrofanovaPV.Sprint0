62a342282cc6d416a95ae2b8c04f8597494e783c
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-09-16 20:08:12 +0500

    Task3

---
+++ Tyuiu.MitrofanovaPV.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
